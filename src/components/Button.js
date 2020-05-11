184e755592786f269405774249ebc637fb923889
Rachel Kat <rachelkat@gmail.com>
2020-05-11 14:38:55 -0400

    saving down work

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 import "components/Button.scss";
+import classNames from 'classnames/bind';
 
 export default function Button(props) {
-   return <></>;
+
+   console.log(props);
+   return <button
+      className={classNames(
+         'button', 
+         { 'button--confirm': props.confirm }, 
+         { 'button--danger': props.danger }
+         )}
+         disabled={props.disabled} 
+         onClick={props.onClick}
+         >{props.children}</button>;
 }
