3694aef125c865448dce5094fb8ad5bcb0c23c0d
Rachel Kat <rachelkat@gmail.com>
2020-05-12 23:05:34 -0400

    displaying appointments exercise

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,8 +1,20 @@
 import React from "react";
-import Header from "./Header.js"
+
 import "./styles.scss";
+
+import Header from "./Header.js";
+import Show from "./Show.js";
+import Empty from "./Empty.js";
 
 export default function Appointment(props) {
   console.log(props);
-  return (<article className="appointment"><Header time={props.time} /></article>);
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {props.interview ?
+        <Show
+          student={props.interview.student}
+          interviewer={props.interview.interviewer} /> :
+        <Empty />}
+    </article>);
 }
