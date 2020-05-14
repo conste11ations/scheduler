0ef46e67d30f8143521071d04c69e5c3423ee535
Rachel Kat <rachelkat@gmail.com>
2020-05-13 22:39:15 -0400

    refactor to combine day days appointments into a single state obj part 1

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,6 +4,7 @@ import axios from "axios";
 import "components/Application.scss";
 import DayList from "components/DayList"
 import Appointment from "components/Appointment";
+import { getAppointmentsForDay } from "helpers/selectors.js";
 
 
 const appointments = [
@@ -56,14 +57,23 @@ const appointments = [
 
 export default function Application(props) {
 
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+  //  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
-  axios({
-    method: 'GET',
-    url: 'http://localhost:8001/api/days'
-  }, [])
-    .then(response => setDays(response.data));
+  useEffect(() => {
+    axios({
+      method: 'GET',
+      url: 'http://localhost:8001/api/days'
+    })
+      .then(response => setDays(response.data));
+  }, []);
 
   return (
     <main className="layout">
@@ -75,8 +85,8 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu"><DayList
-          days={days}
-          day={day}
+          days={state.days}
+          day={state.day}
           setDay={setDay}
         /></nav>
         <img
