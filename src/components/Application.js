1bd95cba9ffc6abe3f8be3f57781ca8a324d69d1
Rachel Kat <rachelkat@gmail.com>
2020-05-14 17:30:54 -0400

    implement custom hook to transition between appointment card states

--- src/components/Application.js
+++ src/components/Application.js
@@ -33,7 +33,6 @@ export default function Application(props) {
       })
     ]).then((all) => {
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
-      console.log(all[2].data);
     })
 
   }, []);
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,20 +1,33 @@
 import React from "react";
+import useVisualMode from "hooks/useVisualMode";
 
 import "./styles.scss";
 
 import Header from "./Header.js";
 import Show from "./Show.js";
 import Empty from "./Empty.js";
+import Form from "./Form.js";
 
 export default function Appointment(props) {
+
+  const EMPTY = "EMPTY";
+  const SHOW = "SHOW";
+  const CREATE = "CREATE";
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
 
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ?
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
-          interviewer={props.interview.interviewer} /> :
-        <Empty />}
+          interviewer={props.interview.interviewer}
+        />
+      )}
+      {mode === CREATE && <Form interviewers={[]} onCancel={() => back()} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
     </article>);
 }
