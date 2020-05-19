c9fcba1784e27b9f9a46ad56759f6aad85cf2992
Rachel Kat <rachelkat@gmail.com>
2020-05-19 02:17:18 -0400

    added error checking for form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -24,6 +24,10 @@ export default function Form(props) {
       setError("Student name cannot be blank");
       return;
     }
+    if (interviewer === null) {
+      setError("Interviewer must be selected");
+      return;
+    }
   
     props.onSave(name, interviewer);
     setError("");
