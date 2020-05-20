88be16471f171d4e87ab12324ed91ed8282debe3
Rachel Kat <rachelkat@gmail.com>
2020-05-19 21:21:57 -0400

    adding more jest tests

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent, waitForElement, getByText, getAllByTestId, getByAltText, getByPlaceholderText, prettyDOM, queryByText  } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -8,4 +8,41 @@ afterEach(cleanup);
 
 xit("renders without crashing", () => {
   render(<Application />);
+});
+
+it("changes the schedule when a new day is selected", async () => {
+  const { getByText } = render(<Application />);
+
+  await waitForElement(() => getByText("Monday"));
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
+});
+
+it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+  const { container } = render(<Application />);
+
+  await waitForElement(() => getByText(container, "Archie Cohen"));
+
+  const appointments = getAllByTestId(container, "appointment");
+  const appointment = appointments[0];
+
+  fireEvent.click(getByAltText(appointment, "Add"));
+  fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+    target: { value: "Lydia Miller-Jones" }
+  });
+  fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  fireEvent.click(getByText(appointment, "Save"));
+
+  //Use the expect function to verify that the the appointment element contains the text "Saving" immediately after the "Save" button is clicked.
+  expect(getByText(appointment, "Saving, please wait...")).toBeInTheDocument();
+  
+  await waitForElement(() => queryByText(appointment, "Lydia Miller-Jones"));
+
+  expect(getByText(appointment, "Lydia Miller-Jones")).toBeInTheDocument();
+        
+    const days = getAllByTestId(container, "day");
+    const day = days.find((element) => queryByText(element, "Monday"));
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
 });
--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -15,18 +15,9 @@ export default function useApplicationData(defaultMode) {
 
   useEffect(() => {
     Promise.all([
-      axios({
-        method: 'GET',
-        url: 'http://localhost:8001/api/days'
-      }),
-      axios({
-        method: 'GET',
-        url: 'http://localhost:8001/api/appointments'
-      }),
-      axios({
-        method: 'GET',
-        url: 'http://localhost:8001/api/interviewers'
-      })
+      axios.get("/api/days"),
+      axios.get("/api/appointments"),
+      axios.get("/api/interviewers")
     ]).then((all) => {
       setState(prev => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     })
@@ -44,13 +35,9 @@ export default function useApplicationData(defaultMode) {
       [id]: appointment
     };
 
-    return axios({
-      method: 'PUT',
-      url: `http://localhost:8001/api/appointments/${id}`,
-      data: {
-        interview
-      }
-    })
+    return axios.put(
+      `/api/appointments/${id}`,
+      { interview })
       .then(() => setState({ ...state, days: spotCounter(state.days, state.day, appointments), appointments }));
   }
 
